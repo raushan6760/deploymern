3bc3005e457a7a8c0a27dc7bd7bb4ba4c70e81f5
unknown <lcs2021032@iiitl.ac.in>
2024-04-10 14:01:21 +0530

    initilal

--- app.js
+++ app.js
@@ -9,17 +9,18 @@ import applicationRouter from "./routes/applicationRoutes.js";
 import {dbConnection} from "./database/dbConnection.js"
 import {errorMiddleware} from "./middlewares/error.js"
 
-const FRONTEND_URL = "https://legendary-narwhal-5a2ed4.netlify.app"
+// const FRONTEND_URL = "https://legendary-narwhal-5a2ed4.netlify.app"
 const app = express()
 dotenv.config({path: "./config/config.env"})
 
-app.use(
-    cors({
-      origin: [FRONTEND_URL],
-      method: ["GET", "POST", "DELETE", "PUT"],
-      credentials: true,
-    })
-  );
+app.use(cors())
+// app.use(
+//     cors({
+//       origin: [FRONTEND_URL],
+//       method: ["GET", "POST", "DELETE", "PUT"],
+//       credentials: true,
+//     })
+//   );
 
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
