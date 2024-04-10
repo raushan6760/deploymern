11bdb58d116858aa9737b825d65433ce5ea30e56
unknown <lcs2021032@iiitl.ac.in>
2024-04-10 12:57:28 +0530

    bjdj

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.use(
     })
   );
 
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+  });
+
 app.use(cookieParser()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
